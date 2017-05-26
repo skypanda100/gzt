aa1a57bc61447175f5a62289eac49c6ede12a37b
skypanda100 <236520700@qq.com>
2017-05-26 21:48:30 +0800

    1.add ajax 2.add sleep.php

--- sleep/sleep.js
+++ sleep/sleep.js
@@ -1,7 +1,7 @@
 /**
  * Created by Administrator on 2017/5/26.
  */
-$(function() {
+/*$(function() {
     // 读取body data-type 判断是哪个页面然后执行相应页面方法，方法在下面。
     var dataType = $('body').attr('data-type');
     console.log(dataType);
@@ -10,335 +10,72 @@ $(function() {
             pageData[key]();
         }
     }
-})
+})*/
 
-// 页面数据
-var pageData = {
-    // ===============================================
-    // 首页
-    // ===============================================
-    'sleep': function sleepData() {
-        var echartsA = echarts.init(document.getElementById('tpl-echarts'));
-        option = {
-            tooltip : {
-                trigger: 'axis',
-                axisPointer : {            // 坐标轴指示器，坐标轴触发有效
-                    type : 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
-                }
+function sleepData(date_r, gg_deep_r, gg_shallow_r, zdt_deep_r, zdt_shallow_r) {
+    var echartsA = echarts.init(document.getElementById('tpl-echarts'));
+    option = {
+        tooltip : {
+            trigger: 'axis',
+            axisPointer : {            // 坐标轴指示器，坐标轴触发有效
+                type : 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+            }
+        },
+        legend: {
+            data:['gg深睡','gg浅睡','zdt深睡','zdt浅睡']
+        },
+        toolbox: {
+            show : true,
+            orient: 'vertical',
+            x: 'right',
+            y: 'center',
+            feature : {
+                mark : {show: true},
+                dataView : {show: true, readOnly: false},
+                magicType : {show: true, type: ['line', 'bar', 'stack', 'tiled']},
+                restore : {show: true},
+                saveAsImage : {show: true}
+            }
+        },
+        calculable : true,
+        xAxis : [
+            {
+                type : 'category',
+                data : date_r
+            }
+        ],
+        yAxis : [
+            {
+                type : 'value'
+            }
+        ],
+        series : [
+            {
+                name:'gg深睡',
+                type:'bar',
+                stack: 'gg',
+                data:gg_deep_r
             },
-            legend: {
-                data:['直接访问','邮件营销','联盟广告','视频广告','搜索引擎','百度','谷歌','必应','其他']
+            {
+                name:'gg浅睡',
+                type:'bar',
+                stack: 'gg',
+                data:gg_shallow_r
             },
-            toolbox: {
-                show : true,
-                orient: 'vertical',
-                x: 'right',
-                y: 'center',
-                feature : {
-                    mark : {show: true},
-                    dataView : {show: true, readOnly: false},
-                    magicType : {show: true, type: ['line', 'bar', 'stack', 'tiled']},
-                    restore : {show: true},
-                    saveAsImage : {show: true}
-                }
+            {
+                name:'zdt深睡',
+                type:'bar',
+                stack: 'zdt',
+                data:zdt_deep_r
             },
-            calculable : true,
-            xAxis : [
-                {
-                    type : 'category',
-                    data : ['周一','周二','周三','周四','周五','周六','周日']
-                }
-            ],
-            yAxis : [
-                {
-                    type : 'value'
-                }
-            ],
-            series : [
-                {
-                    name:'直接访问',
-                    type:'bar',
-                    data:[320, 332, 301, 334, 390, 330, 320]
-                },
-                {
-                    name:'邮件营销',
-                    type:'bar',
-                    stack: '广告',
-                    data:[120, 132, 101, 134, 90, 230, 210]
-                },
-                {
-                    name:'联盟广告',
-                    type:'bar',
-                    stack: '广告',
-                    data:[220, 182, 191, 234, 290, 330, 310]
-                },
-                {
-                    name:'视频广告',
-                    type:'bar',
-                    stack: '广告',
-                    data:[150, 232, 201, 154, 190, 330, 410]
-                },
-                {
-                    name:'搜索引擎',
-                    type:'bar',
-                    data:[862, 1018, 964, 1026, 1679, 1600, 1570],
-                    markLine : {
-                        itemStyle:{
-                            normal:{
-                                lineStyle:{
-                                    type: 'dashed'
-                                }
-                            }
-                        },
-                        data : [
-                            [{type : 'min'}, {type : 'max'}]
-                        ]
-                    }
-                },
-                {
-                    name:'百度',
-                    type:'bar',
-                    barWidth : 5,
-                    stack: '搜索引擎',
-                    data:[620, 732, 701, 734, 1090, 1130, 1120]
-                },
-                {
-                    name:'谷歌',
-                    type:'bar',
-                    stack: '搜索引擎',
-                    data:[120, 132, 101, 134, 290, 230, 220]
-                },
-                {
-                    name:'必应',
-                    type:'bar',
-                    stack: '搜索引擎',
-                    data:[60, 72, 71, 74, 190, 130, 110]
-                },
-                {
-                    name:'其他',
-                    type:'bar',
-                    stack: '搜索引擎',
-                    data:[62, 82, 91, 84, 109, 110, 120]
-                }
-            ]
-        };
+            {
+                name:'zdt浅睡',
+                type:'bar',
+                stack: 'zdt',
+                data:zdt_shallow_r
+            }
+        ]
+    };
 
-
-        echartsA.setOption(option);
-    },
-    // ===============================================
-    // 图表页
-    // ===============================================
-    'chart': function chartData() {
-        // ==========================
-        // 百度图表A http://echarts.baidu.com/
-        // ==========================
-
-        var echartsC = echarts.init(document.getElementById('tpl-echarts-C'));
-
-
-        optionC = {
-            tooltip: {
-                trigger: 'axis'
-            },
-
-            legend: {
-                data: ['蒸发量', '降水量', '平均温度']
-            },
-            xAxis: [{
-                type: 'category',
-                data: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月']
-            }],
-            yAxis: [{
-                type: 'value',
-                name: '水量',
-                min: 0,
-                max: 250,
-                interval: 50,
-                axisLabel: {
-                    formatter: '{value} ml'
-                }
-            },
-                {
-                    type: 'value',
-                    name: '温度',
-                    min: 0,
-                    max: 25,
-                    interval: 5,
-                    axisLabel: {
-                        formatter: '{value} °C'
-                    }
-                }
-            ],
-            series: [{
-                name: '蒸发量',
-                type: 'bar',
-                data: [2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 135.6, 162.2, 32.6, 20.0, 6.4, 3.3]
-            },
-                {
-                    name: '降水量',
-                    type: 'bar',
-                    data: [2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3]
-                },
-                {
-                    name: '平均温度',
-                    type: 'line',
-                    yAxisIndex: 1,
-                    data: [2.0, 2.2, 3.3, 4.5, 6.3, 10.2, 20.3, 23.4, 23.0, 16.5, 12.0, 6.2]
-                }
-            ]
-        };
-
-        echartsC.setOption(optionC);
-
-        var echartsB = echarts.init(document.getElementById('tpl-echarts-B'));
-        optionB = {
-            tooltip: {
-                trigger: 'axis'
-            },
-            legend: {
-                x: 'center',
-                data: ['某软件', '某主食手机', '某水果手机', '降水量', '蒸发量']
-            },
-            radar: [{
-                indicator: [
-                    { text: '品牌', max: 100 },
-                    { text: '内容', max: 100 },
-                    { text: '可用性', max: 100 },
-                    { text: '功能', max: 100 }
-                ],
-                center: ['25%', '40%'],
-                radius: 80
-            },
-                {
-                    indicator: [
-                        { text: '外观', max: 100 },
-                        { text: '拍照', max: 100 },
-                        { text: '系统', max: 100 },
-                        { text: '性能', max: 100 },
-                        { text: '屏幕', max: 100 }
-                    ],
-                    radius: 80,
-                    center: ['50%', '60%'],
-                },
-                {
-                    indicator: (function() {
-                        var res = [];
-                        for (var i = 1; i <= 12; i++) {
-                            res.push({ text: i + '月', max: 100 });
-                        }
-                        return res;
-                    })(),
-                    center: ['75%', '40%'],
-                    radius: 80
-                }
-            ],
-            series: [{
-                type: 'radar',
-                tooltip: {
-                    trigger: 'item'
-                },
-                itemStyle: { normal: { areaStyle: { type: 'default' } } },
-                data: [{
-                    value: [60, 73, 85, 40],
-                    name: '某软件'
-                }]
-            },
-                {
-                    type: 'radar',
-                    radarIndex: 1,
-                    data: [{
-                        value: [85, 90, 90, 95, 95],
-                        name: '某主食手机'
-                    },
-                        {
-                            value: [95, 80, 95, 90, 93],
-                            name: '某水果手机'
-                        }
-                    ]
-                },
-                {
-                    type: 'radar',
-                    radarIndex: 2,
-                    itemStyle: { normal: { areaStyle: { type: 'default' } } },
-                    data: [{
-                        name: '降水量',
-                        value: [2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 75.6, 82.2, 48.7, 18.8, 6.0, 2.3],
-                    },
-                        {
-                            name: '蒸发量',
-                            value: [2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 35.6, 62.2, 32.6, 20.0, 6.4, 3.3]
-                        }
-                    ]
-                }
-            ]
-        };
-        echartsB.setOption(optionB);
-        var echartsA = echarts.init(document.getElementById('tpl-echarts-A'));
-        option = {
-
-            tooltip: {
-                trigger: 'axis',
-            },
-            legend: {
-                data: ['邮件', '媒体', '资源']
-            },
-            grid: {
-                left: '3%',
-                right: '4%',
-                bottom: '3%',
-                containLabel: true
-            },
-            xAxis: [{
-                type: 'category',
-                boundaryGap: true,
-                data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
-            }],
-
-            yAxis: [{
-                type: 'value'
-            }],
-            series: [{
-                name: '邮件',
-                type: 'line',
-                stack: '总量',
-                areaStyle: { normal: {} },
-                data: [120, 132, 101, 134, 90, 230, 210],
-                itemStyle: {
-                    normal: {
-                        color: '#59aea2'
-                    },
-                    emphasis: {
-
-                    }
-                }
-            },
-                {
-                    name: '媒体',
-                    type: 'line',
-                    stack: '总量',
-                    areaStyle: { normal: {} },
-                    data: [220, 182, 191, 234, 290, 330, 310],
-                    itemStyle: {
-                        normal: {
-                            color: '#e7505a'
-                        }
-                    }
-                },
-                {
-                    name: '资源',
-                    type: 'line',
-                    stack: '总量',
-                    areaStyle: { normal: {} },
-                    data: [150, 232, 201, 154, 190, 330, 410],
-                    itemStyle: {
-                        normal: {
-                            color: '#32c5d2'
-                        }
-                    }
-                }
-            ]
-        };
-        echartsA.setOption(option);
-    }
+    echartsA.setOption(option);
 }
---
+++ sleep/sleep.php
@@ -0,0 +1,129 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/5/26
+ * Time: 19:59
+ */
+
+require_once "../db/pgsql.php";
+
+$start_date = null;
+if(isset($_POST["start_date"]))
+{
+    $start_date = $_POST["start_date"];
+}
+
+$end_date = null;
+if(isset($_POST["end_date"]))
+{
+    $end_date = $_POST["end_date"];
+}
+
+$db = new pgsql("192.168.1.3", "15432", "postgres", "postgres", "123456");
+$db->connect();
+$sql = "";
+if(!is_null($start_date) && !is_null($end_date))
+{
+    $sql = "select * from status_sleep where date >= '$start_date'and date <= '$end_date'order by date";
+
+}
+else if(!is_null($start_date) && is_null($end_date))
+{
+    $sql = "select * from status_sleep where date >= '$start_date' order by date";
+}
+else if(is_null($start_date) && !is_null($end_date))
+{
+    $sql = "select * from status_sleep where date <= '$end_date'order by date";
+}
+else
+{
+    $sql = "select * from status_sleep order by date";
+}
+
+$date_r = array();
+$gg_deep_r = array();
+$gg_shallow_r = array();
+$zdt_deep_r = array();
+$zdt_shallow_r = array();
+
+$result = $db->query($sql);
+while(($row = $db->fetchRow()) != NULL)
+{
+    $person = $row[0];
+    $date = $row[1];
+    $start = $row[2];
+    $end = $row[3];
+    $deepStart01 = $row[4];
+    $deepEnd01 = $row[5];
+    $deepStart02 = $row[6];
+    $deepEnd02 = $row[7];
+    $deepStart03 = $row[8];
+    $deepEnd03 = $row[9];
+    $deepStart04 = $row[10];
+    $deepEnd04 = $row[11];
+    $deepStart05 = $row[12];
+    $deepEnd05 = $row[13];
+    $deepStart06 = $row[14];
+    $deepEnd06 = $row[15];
+    $deepStart07 = $row[16];
+    $deepEnd07 = $row[17];
+    $deepStart08 = $row[18];
+    $deepEnd08 = $row[19];
+    $deepStart09 = $row[20];
+    $deepEnd09 = $row[21];
+    $deepStart10 = $row[22];
+    $deepEnd10 = $row[23];
+    $awakeStart01 = $row[24];
+    $awakeEnd01 = $row[25];
+    $awakeStart02 = $row[26];
+    $awakeEnd02 = $row[27];
+    $awakeStart03 = $row[28];
+    $awakeEnd03 = $row[29];
+    $awakeStart04 = $row[30];
+    $awakeEnd04 = $row[31];
+
+    $sum = strtotime($end) - strtotime($start);
+
+    $deep = strtotime($deepEnd01) - strtotime($deepStart01);
+    $deep += strtotime($deepEnd02) - strtotime($deepStart02);
+    $deep += strtotime($deepEnd03) - strtotime($deepStart03);
+    $deep += strtotime($deepEnd04) - strtotime($deepStart04);
+    $deep += strtotime($deepEnd05) - strtotime($deepStart05);
+    $deep += strtotime($deepEnd06) - strtotime($deepStart06);
+    $deep += strtotime($deepEnd07) - strtotime($deepStart07);
+    $deep += strtotime($deepEnd08) - strtotime($deepStart08);
+    $deep += strtotime($deepEnd09) - strtotime($deepStart09);
+    $deep += strtotime($deepEnd10) - strtotime($deepStart10);
+
+    $awake = strtotime($awakeEnd01) - strtotime($awakeStart01);
+    $awake += strtotime($awakeEnd02) - strtotime($awakeStart02);
+    $awake += strtotime($awakeEnd03) - strtotime($awakeStart03);
+    $awake += strtotime($awakeEnd04) - strtotime($awakeStart04);
+
+    $shallow = $sum - $deep - $awake;
+
+    if($person == 0)
+    {
+        array_push($gg_deep_r, $deep / 60);
+        array_push($gg_shallow_r, $shallow / 60);
+    }
+    else
+    {
+        array_push($zdt_deep_r, $deep / 60);
+        array_push($zdt_shallow_r, $shallow / 60);
+    }
+
+    $date_s = substr($date, 0, 10);
+    if(!in_array($date_s, $date_r, true))
+    {
+        array_push($date_r, $date_s);
+    }
+}
+
+$db->free();
+
+$data_r = array("date" => $date_r, "gg_deep" => $gg_deep_r, "gg_shallow" => $gg_shallow_r, "zdt_deep" => $zdt_deep_r, "zdt_shallow" => $zdt_shallow_r);
+
+echo json_encode($data_r);
+?>
