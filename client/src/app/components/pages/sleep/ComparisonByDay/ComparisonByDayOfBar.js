7aee8ef151abe6661f67c472b9e6eab92b6d07fc
skypanda100 <236520700@qq.com>
2017-06-04 19:18:50 +0800

    1. add day module

---
+++ client/src/app/components/pages/sleep/ComparisonByDay/ComparisonByDayOfBar.js
@@ -0,0 +1,147 @@
+/**
+ * Created by Administrator on 2017/6/4.
+ */
+import React, {Component} from 'react';
+import withWidth, {MEDIUM, LARGE} from 'material-ui/utils/withWidth';
+import echarts from "echarts";
+import 'whatwg-fetch';
+
+class ComparisonByDayOfBar extends Component {
+
+    constructor () {
+        super();
+    }
+
+    post(postData, dispatch) {
+        fetch('http://192.168.1.3:8765/gzt/server/sleep/comparison_by_day/comparison_by_day_of_bar.php', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: postData
+        })
+            .then(response => response.json())
+            .then(json => {
+                dispatch({
+                    message: json,
+                    isSuccess: true
+                });
+            })
+            .catch((err) => {
+                dispatch({
+                    message: err.message,
+                    isSuccess: false
+                });
+            });
+        /*fetch('/day.json')
+            .then(response => response.json())
+            .then(json => {
+                dispatch({
+                    message: json,
+                    isSuccess: true
+                });
+            })
+            .catch((err) => {
+                dispatch({
+                    message: err.message,
+                    isSuccess: false
+                });
+            });*/
+    }
+
+    draw(data){
+        var date;
+        var gg_deep;
+        var gg_shallow;
+        var zdt_deep;
+        var zdt_shallow;
+        if(data.isSuccess){
+            var data_r = data.message;
+            date = data_r.date_day[data_r.date_day.length - 1];
+            gg_deep = data_r.gg_deep_day[data_r.gg_deep_day.length - 1];
+            gg_shallow = data_r.gg_shallow_day[data_r.gg_shallow_day.length - 1];
+            zdt_deep = data_r.zdt_deep_day[data_r.zdt_deep_day.length - 1];
+            zdt_shallow = data_r.zdt_shallow_day[data_r.zdt_shallow_day.length - 1];
+        }
+
+        var chart = echarts.init(document.getElementById('bar'));
+
+        var option = {
+            title: {
+                text: '',
+                subtext: date
+            },
+            tooltip: {
+                trigger: 'axis',
+                axisPointer: {
+                    type: 'shadow'
+                },
+                formatter: function(params) {
+                    // for text color
+                    var res = '<strong>' + params[0].name + '</strong>'
+                    for (var i = 0, l = params.length; i < l; i++) {
+                        res += '<br/>' + params[i].seriesName + ' : ' + params[i].value.toFixed(2)
+                    }
+                    res += '</div>';
+                    return res;
+                }
+            },
+            legend: {
+                x: 'right',
+                data:['GG','ZDT']
+            },
+            toolbox: {
+                show: false,
+                orient: 'vertical',
+                y: 'center',
+                feature: {
+                    mark: {show: true},
+                }
+            },
+            calculable: true,
+            grid: {
+                y: 50,
+                y2: 25,
+                x2: 6
+            },
+            xAxis: [
+                {
+                    type: 'category',
+                    data: ['总睡眠', '深睡眠']
+                }
+            ],
+            yAxis: [
+                {
+                    type: 'value'
+                }
+            ],
+            series: [
+                {
+                    name: 'GG',
+                    type: 'bar',
+                    data: [gg_deep + gg_shallow, gg_deep]
+                },
+                {
+                    name: 'ZDT',
+                    type: 'bar',
+                    data: [zdt_deep + zdt_shallow, zdt_deep]
+                }
+            ]
+        };
+
+        chart.setOption(option);
+    }
+
+    componentDidMount() {
+        this.post('', this.draw);
+    }
+
+    render () {
+        return (
+            <div id='bar' style={{width:'100%',height:'400px'}}>
+            </div>
+        )
+    }
+}
+
+export default withWidth()(ComparisonByDayOfBar);
--- client/src/app/components/pages/sleep/ComparisonByDay/Page.js
+++ client/src/app/components/pages/sleep/ComparisonByDay/Page.js
@@ -1,18 +1,42 @@
 import React from 'react';
 import Title from 'react-title-component';
 import DataBlock from '../../../blocks/index';
-import ComparisonByDay from './ComparisonByDay';
-import comparisonByDayCode from '!raw!./ComparisonByDay';
+import ComparisonByDayOfLine from './ComparisonByDayOfLine';
+import ComparisonByDayOfLineCode from '!raw!./ComparisonByDayOfLine';
+import ComparisonByDayOfTemperature from './ComparisonByDayOfTemperature';
+import ComparisonByDayOfTemperatureCode from '!raw!./ComparisonByDayOfTemperature';
+import ComparisonByDayOfBar from './ComparisonByDayOfBar';
+import ComparisonByDayOfBarCode from '!raw!./ComparisonByDayOfBar';
+import ComparisonByDayOfPie from './ComparisonByDayOfPie';
+import ComparisonByDayOfPieCode from '!raw!./ComparisonByDayOfPie';
 
 const ComparisonByDayPage = () => (
   <div>
-    <Title render={(previousTitle) => `Comparison By Day - ${previousTitle}`} />
-    <DataBlock
-      code={comparisonByDayCode}
-      title="Simple example"
-    >
-        <ComparisonByDay />
-    </DataBlock>
+      <Title render={(previousTitle) => `Comparison By Day - ${previousTitle}`} />
+      <DataBlock
+          code={ComparisonByDayOfLineCode}
+          title="Line"
+      >
+          <ComparisonByDayOfLine />
+      </DataBlock>
+      <DataBlock
+          code={ComparisonByDayOfTemperatureCode}
+          title="Temperature"
+      >
+          <ComparisonByDayOfTemperature />
+      </DataBlock>
+      <DataBlock
+          code={ComparisonByDayOfBarCode}
+          title="Bar"
+      >
+          <ComparisonByDayOfBar />
+      </DataBlock>
+      <DataBlock
+          code={ComparisonByDayOfPieCode}
+          title="Pie"
+      >
+          <ComparisonByDayOfPie />
+      </DataBlock>
   </div>
 );
 
